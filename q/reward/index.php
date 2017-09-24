7bf965aa33f461a897fa8dc3f3e19cc8597c1655
FreelaBit <31524456+freelacoin@users.noreply.github.com>
2017-09-24 18:41:36 -0300

    Freelabit

--- q/reward/index.php
+++ q/reward/index.php
@@ -1,6 +1,6 @@
 <?php 
 $data_string = '{"jsonrpc":"2.0","id":"test","method":"getlastblockheader","params":" "}';
-$ch = curl_init('http://0.0.0.0:24888/json_rpc');
+$ch = curl_init('http://155.254.33.76:31006/json_rpc');
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
