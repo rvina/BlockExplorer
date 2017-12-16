aedd03d5f90f57652a885ef1d39fac7b055924b6
FreelaBit <31524456+freelacoin@users.noreply.github.com>
2017-12-16 14:27:19 -0200

    Updated FreelaBit ticker

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
-var api = 'http://155.254.33.76:31006';
+var api = 'http://XXX.XXXX.XX.XX:31006'; //replace X for the IP where daemon is hosted or localhost
 var blockTargetInterval = 120;
 var coinUnits = 1000000;
-var symbol = 'FLB';
+var symbol = 'FBF';
 var refreshDelay = 30000;
 // pools stats by MainCoins
 var networkStat = {
-    "flb": [
+    "fbf": [
         ["", ""],
 
     ]
